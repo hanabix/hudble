c78da6ddf256d67609e78218148885f4c6b5c28a
zhongl <zhong.lunfu@gmail.com>
2026-04-10 23:04:43 +0800

    fix robolectric sdk for ci

--- app/src/test/java/hanabix/hubu/ble/AndroidConnectCallbackTest.kt
+++ app/src/test/java/hanabix/hubu/ble/AndroidConnectCallbackTest.kt
@@ -20,7 +20,7 @@ import org.robolectric.annotation.Config
 import java.util.UUID
 
 @RunWith(RobolectricTestRunner::class)
-@Config(manifest = Config.NONE)
+@Config(manifest = Config.NONE, sdk = [32])
 class AndroidConnectCallbackTest {
 
     @Test
